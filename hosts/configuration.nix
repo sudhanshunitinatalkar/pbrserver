4e48eca7c077a0234bf9a250532342051e3db249
sudhanshunitinatalkar <atalkarsudhanshu@proton.me>
2025-11-06 21:39:08 +0530

    added thingsboard server

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -62,6 +62,11 @@
 
       authentication = 
       ''
+        # These lines are required for NixOS's own scripts
+        local all postgres peer
+        local all all      peer
+        
+        # These lines allow ThingsBoard to connect with a password
         local all all md5
         host  all all 127.0.0.1/32 md5
         host  all all ::1/128      md5
